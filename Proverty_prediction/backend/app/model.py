6808ba86aef514ccbd85f1bdc7d787773f4f0d30
PredictiveResearchPB <nischal.predictiveresearch@gmail.com>
2026-01-04 05:13:15 +0530

    working

--- Proverty_prediction/backend/app/model.py
+++
@@ -1,7 +0,0 @@
-import os
-import joblib
-
-def load_model():
-    BASE_DIR = os.path.dirname(__file__)
-    model_path = os.path.join(BASE_DIR, "../artifacts/titanic_pipeline.pkl")
-    return joblib.load(model_path)
